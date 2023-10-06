7661ef7a071ef35443ac51eecfac649f93b6a936
Ousmane Diallo <145231625+Ousmanediallo2026@users.noreply.github.com>
2023-10-06 03:41:00 +0000

    Chnages todays

--- script/script.js
+++ script/script.js
@@ -23,7 +23,7 @@ loginButton.addEventListener("click", function (){
 const depositButton = document.getElementById("deposit-btn");
 depositButton.addEventListener("click", function(){
    
-   const amount = document.getElementById("deposit-amount").value;
+   const amount = document.getElementById("number").value;
    const amountNumber = parseFloat(amount);
 
 
@@ -31,8 +31,35 @@ depositButton.addEventListener("click", function(){
    const currentNumber = parseFloat(currentBalance);
    const total = currentNumber + amountNumber;
    document.getElementById("current-balance").innerText = total;
+  
+   // Update the curent deposit anytime the user deposit money.
+   const currentBalance1 = document.getElementById("deposit-amount").innerText
+   const currentNumber1 = parseFloat(currentBalance1);
+   const total1 = currentNumber1 + amountNumber;
+   document.getElementById("deposit-amount").innerText = total1;
+   console.log(total1)
+  
 })
 
+  // Withdraw code line 
   
+
+  const withdrawButton = document.getElementById("withdraw-btn");
+  withdrawButton.addEventListener("click", function(){
+   const currentWithdraw = document.getElementById("number-1").value
+   const amountWithdraw = parseFloat(currentWithdraw);
+
+   const theWithdraw = document.getElementById("current-balance").innerText
+   const currentThisWithdaw = parseFloat(theWithdraw);
+   if (amountWithdraw < currentThisWithdaw){
+      alert("You have unsifficient balance for this transaction.")
+   } else {
+      const total2 = currentThisWithdaw - amountWithdraw;
+      document.getElementById("current-balance").innerText = total2;
+      console.log(total2)
+
+   }
+ 
+  })
    
 })
